1453a3c10a6803f979df04035ea6b32ff040d1b2
lynxtrakker <82948981+lynxtrakker@users.noreply.github.com>
2021-05-06 13:16:58 -0500

    Removed logging from message.js file

--- events/message.js
+++ events/message.js
@@ -1,28 +1,6 @@
-const fs = require('fs');
-
 module.exports = {
   name: 'message',
   execute(message) {
-    const fs = require('fs');
-    const { Client, Intents, MessageEmbed } = require('discord.js');
-    const logFolder = fs.readdirSync('./logs');
-    const logFiles = fs.readdirSync('./logs').filter(file => file.endsWith('.log'));
-    if (message.channel.type === 'dm') return;
-    var y = message.guild.id
-    var x = Date()
-    var z = new Date().toLocaleString();
-    //var txt = new ActiveXObject("Scripting.FileSystemObject");
-    //var s = txt.CreateTextFile(`${y}.log`, true);
-    
-    if (!message.channel.id === `./logs/${y}.log`) {
-      CreateTextFile(`${y}.log`, true); 
-    }
-    
     console.log(`${message.author.id} in #${message.channel.id} in ${message.guild.id} sent: ${message.content}`);
- 
-    fs.appendFile(`${y}.log`, `${z}, ${message.channel.name} ${message.author.tag}(${message.author.id}): ${message.content}\n`, (err) => {
-      if (err) throw err;
-    });
-    
   },
 };
